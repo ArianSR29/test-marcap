ec4647aa3cf04acf4c53ad2ff055dabd15542c36
Rodrigo Arenas <arenasgonzalezrodrigo@gmail.com>
2019-01-10 20:53:39 -0600

    feat: upgrade necesary modules
    Author: Rodrigo Arenas G <arenasgonzalezrodrgio@gmail.com>

--- Scripts/Activate.ps1
+++ Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Projects\python\test-marcap"
+$env:VIRTUAL_ENV="C:\Projects\Python\test-marcap"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
